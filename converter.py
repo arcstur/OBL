8e03d315397e861d731ebc94c030ea0450a4afdb
arcstur <arcstur@gmail.com>
2022-04-23 15:10:31 -0300

    remove: old files

--- converter.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import glob
-
-# Lista todos os nomes de arquivos .xlsx
-xlsx_file_names = glob.glob('*.xlsx')
-
-# Para cada um, converte para CSV
-for f in xlsx_file_names:
-    pd.read_excel(f).to_csv(f.replace('xlsx', 'csv'), index=None, header=True)
--- main.py
+++ main.py
@@ -1,17 +1,9 @@
 from analise_olimpica import objects, multiple
 
-
-
 def main():
-    print()
     school_dict_object = objects.SchoolDict.from_school_dict_csv()
 
     multiple.analyse_multiple_exams('20220105_GERAL2019-2020-2021.csv', school_dict_object)
 
-    
-
-
 if __name__ == '__main__':
     main()
-
-
